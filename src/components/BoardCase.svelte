951a365ed40dcef77e6b02bd1dd7a2b12dae523a
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2025-07-30 12:26:23 +0200

    Create HTML grid game board

--- src/components/BoardCase.svelte
+++ src/components/BoardCase.svelte
@@ -1,16 +1,24 @@
-<!-- case individuelle -->
-
+<!-- case individuelle en grille -->
 <script lang="ts">
   export let id: number;
-  export let x: number;
-  export let y: number;
   export let color: string;
   export let icon: string | null | undefined = undefined;
 </script>
 
-<g transform={`translate(${x}, ${y})`}>
-  <rect width="36" height="36" rx="6" fill={color} stroke="black" />
-  <text x="18" y="24" text-anchor="middle" fill="white" font-size="16">
-    {icon ?? id + 1}
-  </text>
-</g>
+<div class="case" style="background-color: {color}">
+  {icon ?? id + 1}
+</div>
+
+<style>
+  .case {
+    width: 36px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    border-radius: 6px;
+    color: white;
+    font-size: 16px;
+  }
+</style>
